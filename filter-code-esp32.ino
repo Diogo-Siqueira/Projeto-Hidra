80f080c045f3a6d77b6bb35a8711611083ccbcb5
Diogo Siqueira <126920478+Diogo-Siqueira@users.noreply.github.com>
2024-05-16 00:23:44 -0300

    Update filter-code-esp32.ino
    novo

--- filter-code-esp32.ino
+++ filter-code-esp32.ino
@@ -1,80 +1,37 @@
-#define TdsSensorPin 27
-#define VREF 3.3              // analog reference voltage(Volt) of the ADC
-#define SCOUNT  1            // sum of sample point
+#include <EEPROM.h>
+#include "GravityTDS.h"
+#include <LiquidCrystal.h>
 
-int analogBuffer[SCOUNT];     // store the analog value in the array, read from ADC
-int analogBufferTemp[SCOUNT];
-int analogBufferIndex = 0;
-int copyIndex = 0;
+LiquidCrystal lcd(22,21,5,18,23,19);
 
-float averageVoltage = 0;
-float tdsValue = 0;
-float temperature = 25;       // current temperature for compensation
+#define TdsSensorPin 32
+GravityTDS gravityTds;
 
-// median filtering algorithm
-int getMedianNum(int bArray[], int iFilterLen){
-  int bTab[iFilterLen];
-  for (byte i = 0; i<iFilterLen; i++)
-  bTab[i] = bArray[i];
-  int i, j, bTemp;
-  for (j = 0; j < iFilterLen - 1; j++) {
-    for (i = 0; i < iFilterLen - j - 1; i++) {
-      if (bTab[i] > bTab[i + 1]) {
-        bTemp = bTab[i];
-        bTab[i] = bTab[i + 1];
-        bTab[i + 1] = bTemp;
-      }
-    }
-  }
-  if ((iFilterLen & 1) > 0){
-    bTemp = bTab[(iFilterLen - 1) / 2];
-  }
-  else {
-    bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
-  }
-  return bTemp;
+float temperature = 25,tdsValue = 0;
+ 
+void setup()
+{
+    Serial.begin(115200);
+    lcd.begin(16,2);
+    gravityTds.setPin(TdsSensorPin);
+    gravityTds.setAref(3.0);  //reference voltage on ADC, default 5.0V on Arduino UNO
+    gravityTds.setAdcRange(4096);  //1024 for 10bit ADC;4096 for 12bit ADC
+    gravityTds.begin();  //initialization
 }
-
-void setup(){
-  Serial.begin(300);
-  pinMode(TdsSensorPin, INPUT);
-}
-
-void loop(){
-  static unsigned long analogSampleTimepoint = millis();
-  if(millis() - analogSampleTimepoint > 40U){     //every 40 milliseconds,read the analog value from the ADC
-    analogSampleTimepoint = millis();
-    int sensorValue = analogRead(TdsSensorPin);
-    Serial.print("Raw Sensor Value: ");
-    Serial.println(sensorValue);
-    analogBuffer[analogBufferIndex] = sensorValue;    //read the analog value and store into the buffer
-    analogBufferIndex++;
-    if(analogBufferIndex == SCOUNT){ 
-      analogBufferIndex = 0;
-    }
-  }   
-
-  static unsigned long printTimepoint = millis();
-  if(millis() - printTimepoint > 2000U){ // 2 seconds interval
-    printTimepoint = millis();
-
-    analogBufferTemp[copyIndex] = analogBuffer[copyIndex];
-
-    // read the analog value more stable by the median filtering algorithm, and convert to voltage value
-    averageVoltage = getMedianNum(analogBufferTemp, SCOUNT) * (float)VREF / 4096.0;
-    Serial.print("Average Voltage: ");
-    Serial.print(averageVoltage);
-    Serial.println(" V");
-
-    // temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0)); 
-    float compensationCoefficient = 1.0 + 0.02 * (temperature - 25.0);
-    // temperature compensation
-    float compensationVoltage = averageVoltage / compensationCoefficient;
-   
-    // convert voltage value to tds value
-    tdsValue = (133.42 * compensationVoltage * compensationVoltage * compensationVoltage - 255.86 * compensationVoltage * compensationVoltage + 857.39 * compensationVoltage) * 0.5;
-    Serial.print("TDS Value: ");
-    Serial.print(tdsValue, 0);
-    Serial.println(" ppm");
-  }
+ 
+void loop()
+{
+    //temperature = readTemperature();  //add your temperature sensor and read it
+    gravityTds.setTemperature(temperature);  // set the temperature and execute temperature compensation
+    gravityTds.update();  //sample and calculate
+    tdsValue = gravityTds.getTdsValue();  // then get the value
+    Serial.print(tdsValue,0);
+    Serial.println("ppm");
+    lcd.setCursor(0, 0);
+    lcd.print("TDS Value:");
+    lcd.setCursor(0, 1);
+    lcd.print(tdsValue,0);
+    lcd.print(" PPM");
+    delay(1000);
+    lcd.clear();
 }
